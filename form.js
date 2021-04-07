ad56e4bb61e56a44d12102be70dc438a8266ead7
luciana-fragoso <luhciana@gmail.com>
2021-04-07 08:57:30 +0100

    Adding JS

--- form.js
+++ form.js
@@ -1,6 +1,10 @@
 window.onload = function() {
     
     var myButton = document.getElementById('my-button');
+    var buttonForm = document.getElementById("btn-form");
+    var buttonMandatory = document.getElementById("btn-mandatory");
+    var modalForm = document.getElementById("modal-form");
+    var modalRequire = document.getElementById("modal-mandatory");
     
     myButton.onclick = function(event){
         
@@ -12,38 +16,33 @@ window.onload = function() {
         var fields = ["surname","firstname","telephone","email","message"];
 
         
-        if (checkEmptyFields(fields)){
+    
             if (validateTelephone(telephone) && validateEmail(email)){
                 for (field of fields) {
                     let input = field;
                     let value = document.getElementById(field).value;
                     if (input === "telephone"){
                         if (!(value === ""|| (!value.trim().length)))
-                            console.log(input.charAt(0).toUpperCase() + input.slice(1)+": "+value);
+                            console.log(input.toUpperCase()+": "+value);
                     } else {
-                        console.log(input.charAt(0).toUpperCase() + input.slice(1)+": "+value);
+                        console.log(input.toUpperCase()+": "+value);
                     }
                 }
-                    
+                modalForm.style.display = "block";
                 emptyFields(fields);      
             }   
-        }
+        
+    }
+
+    buttonForm.onclick = function() {
+       modalForm.style.display = "none";
+      }
+    
+    buttonMandatory.onclick = function() {
+        modalRequire.style.display = "none";
     }
 }
 
-function checkEmptyFields(fields){ 
-    for (field of fields){
-        if (field !== "telephone"){
-            let v = document.getElementById(field).value;
-            if (v === "" || (!v.trim().length)){
-                alert("All fields marked * are mandatory");
-                return false;
-            }
-        }
-        
-    }
-    return true;
-}
 
 function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -63,3 +62,34 @@ function  emptyFields(fields){
         document.getElementById(field).value = "";
 }
 
+
+
+
+  window.onclick = function(event) {
+    var modalForm = document.getElementById("modal-form");
+    var modalRequire = document.getElementById("modal-mandatory");
+
+    if (event.target == modalForm) {
+        modalForm.style.display = "none";
+    }
+
+    if (event.target == modalRequire) {
+        modalRequire.style.display = "none";
+      }
+  }
+
+
+  /*
+function checkEmptyFields(fields,modalRequire){ 
+    for (field of fields){
+        if (field !== "telephone"){
+            let v = document.getElementById(field).value;
+            if (v === "" || (!v.trim().length)){
+                modalRequire.style.display = "block";
+                return false;
+            }
+        }
+        
+    }
+    return true;
+}*/
